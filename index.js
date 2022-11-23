19365629d195e7ac114d654fce6eea4b7351c4af
Keshav Sharma <keshav2743@gmail.com>
2022-11-23 15:37:02 +0530

    sendFile to client index.html

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const dbUrl = process.env.DB_URL;
 if(process.env.NODE_ENV !== "production")
 {
   
-   app.use(express.static('client/build'));
+   app.use('/',express.static('client/build'));
    app.get('*' , (req , res)=>{
 
     res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
