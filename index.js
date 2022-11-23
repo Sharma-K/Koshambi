838840e5ffeff04ad16d9ca0dd48120a21a913b2
Keshav Sharma <keshav2743@gmail.com>
2022-11-23 15:20:10 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-if(process.env.NODE_ENV !== "production")
-{
-    require('dotenv').config();
-}
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -15,7 +12,11 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongo');
 const port = process.env.PORT || 5000;
 const dbUrl = process.env.DB_URL;
-
+if(process.env.NODE_ENV !== "production")
+{
+  
+   app.use('/', express.static('client/build'))
+}
 const store =  MongoDBStore.create({
   mongoUrl: dbUrl,
   crypto:{
