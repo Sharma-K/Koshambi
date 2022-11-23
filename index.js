a4970cd0c9c26d6518acb8839c26dc9e48597b4e
Keshav Sharma <keshav2743@gmail.com>
2022-11-23 15:35:03 +0530

    sendFile to client index.html

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const User = require('./model/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const path = require('path');
 
 const session = require('express-session');
 const MongoDBStore = require('connect-mongo');
@@ -15,7 +16,12 @@ const dbUrl = process.env.DB_URL;
 if(process.env.NODE_ENV !== "production")
 {
   
-   app.use('/', express.static('client/build'))
+   app.use(express.static('client/build'));
+   app.get('*' , (req , res)=>{
+
+    res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+
+})
 }
 const store =  MongoDBStore.create({
   mongoUrl: dbUrl,
